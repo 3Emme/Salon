447d8750ea25788019dc025103fbaddae805e5bf
3Emme <emmettbuentiempo@gmail.com>
2020-10-09 13:01:43 -0700

    write edit get and post paths

--- HairSalon/Controllers/ClientsController.cs
+++ HairSalon/Controllers/ClientsController.cs
@@ -12,14 +12,14 @@ namespace HairSalon.Controllers
     private readonly HairSalonContext _db;
     public ClientsController(HairSalonContext db)
     {
-        _db = db;
+      _db = db;
     }
     public ActionResult Create(int id)
-      {
-        ViewBag.StylistId = id;
-        return View();
-      }
-      [HttpPost]
+    {
+      ViewBag.StylistId = id;
+      return View();
+    }
+    [HttpPost]
     public ActionResult Create(Client client )
     {
       _db.Clients.Add(client);
@@ -27,9 +27,22 @@ namespace HairSalon.Controllers
       return RedirectToAction("Create");
     }
     public ActionResult Details(int id)
-      {
-        Client model =  _db.Clients.FirstOrDefault(x => x.ClientId == id);
-        return View(model);
-      }
+    {
+      Client model =  _db.Clients.FirstOrDefault(x => x.ClientId == id);
+      return View(model);
+    }
+    public ActionResult Edit(int id)
+    {
+      ViewBag.StylistId = new SelectList(_db.Stylists, "StylistId", "StylistName");
+      var thisClient = _db.Clients.FirstOrDefault(x => x.ClientId == id);
+      return View(thisClient);
+    }
+    [HttpPost]
+    public ActionResult Edit(Client client)
+    {
+      _db.Entry(client).State = EntityState.Modified;
+      _db.SaveChanges();
+      return RedirectToAction("Edit");
+    }
   }
 }
