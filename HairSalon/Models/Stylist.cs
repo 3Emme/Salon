8788f2ac8d537ac6569610aa5ec0743df9d66d6e
3Emme <emmettbuentiempo@gmail.com>
2020-10-09 10:35:26 -0700

    write stylist model

---
+++ HairSalon/Models/Stylist.cs
@@ -0,0 +1,13 @@
+namespace HairSalon.Models
+{
+    public class Stylist
+    {
+        public int StylistId {get;set;}
+        public string StylistName {get;set;}
+        public string StylistDetails {get;set;}
+        public Stylist()
+        {
+          //
+        }
+    }
+}
